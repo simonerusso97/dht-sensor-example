2856d75e099a299a90aa6e17bf221807edd3e0f8
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-10-06 17:31:54 +0200

    Cambio libreria
    Passaggio dalla libreria http a axios

--- sensor-test.js
+++ sensor-test.js
@@ -1,20 +1,25 @@
 const sensorLib = require('node-dht-sensor');
-const http = require('http')
+const http = require('http');
+const axios = require('axios');
 
 
 // Setup sensor, exit if failed
 var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
-var sensorPin  = 4;  // The GPIO pin number for sensor signal
+var sensorPin = 4;  // The GPIO pin number for sensor signal
+
 if (!sensorLib.initialize(sensorType, sensorPin)) {
     console.warn('Failed to initialize sensor');
     process.exit(1);
 }
+else{
+    console.log('sensor ok');
+}
 
 // Automatically update sensor value every 2 seconds
-setInterval(function() {
+setInterval(function () {
     var readout = sensorLib.read();
     console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
-    console.log('Humidity:   ', readout.humidity.toFixed(1)    + '%');
+    console.log('Humidity:   ', readout.humidity.toFixed(1) + '%');
 
     const data = JSON.stringify({
         "sensor": "ID1",
@@ -22,31 +27,43 @@ setInterval(function() {
         "temperature": readout.temperature.toFixed(1),
     });
 
-    const options = {
-        hostname: '192.168.1.100', //TODO: cambia a seconda quale rete wifi uso
-        port: 3000,
-        path: '/temperature',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': data.length
-        }
-    };
-
-    const req = http.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`);
-
-        res.on('data', d => {
-            process.stdout.write(d)
+    axios
+        .post('http://192.168.1.100/temperature', data)
+        .then(res => {
+            console.log(`statusCode: ${res.status}`)
+            console.log(res)
+        })
+        .catch(error => {
+            console.error(error)
         });
 
-        req.on('error', error => {
-            console.log(error)
-        });
+
+    //req.write(data)
+
+
+});
+
+const options = {
+    hostname: '192.168.1.100', //TODO: cambia a seconda quale rete wifi uso
+    port: 3000,
+    path: '/temperature',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+    }
+};
+const req = http.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on('data', d => {
+        process.stdout.write(d)
     });
 
-    req.write(data)
-    req.end()
+    req.on('error', error => {
+        console.log(error)
+    });
+    //req.end()
 }, 2000);
 
 
