427d9fcc8da5f3216e15fa65571bf276982adcf7
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-10-06 18:21:33 +0200

    Update sensor-test.js

--- sensor-test.js
+++ sensor-test.js
@@ -1,7 +1,6 @@
 const sensorLib = require('node-dht-sensor');
 const http = require('http')
 
-
 // Setup sensor, exit if failed
 var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
 var sensorPin  = 4;  // The GPIO pin number for sensor signal
@@ -10,45 +9,44 @@ if (!sensorLib.initialize(sensorType, sensorPin)) {
     process.exit(1);
 }
 
+const options = {
+    hostname: '192.168.1.251',
+    port: 3000,
+    path: '/temperature',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+    }
+}
+
+const req = http.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+    res.on('data', d => {
+        process.stdout.write(d)
+    });
+    req.on('error', error => {
+        console.log(error)
+    });
+
+});
+
+const data = JSON.stringify({
+    "sensor":"ID1",
+    "timestamp": 123456789,
+    "temperature":20
+});
+
 // Automatically update sensor value every 2 seconds
 setInterval(function() {
     var readout = sensorLib.read();
+
+    console.log('invio');
+
     console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
     console.log('Humidity:   ', readout.humidity.toFixed(1)    + '%');
 
-    const data = JSON.stringify({
-        "sensor": "ID1",
-        "timestamp": "0",
-        "temperature": readout.temperature.toFixed(1),
-    });
+    req.write(data);
+    req.end();
 
-    const options = {
-        hostname: '192.168.1.100', //TODO: cambia a seconda quale rete wifi uso
-        port: 3000,
-        path: '/temperature',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': data.length
-        }
-    };
-
-    const req = http.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`);
-
-        res.on('data', d => {
-            process.stdout.write(d)
-        });
-
-        req.on('error', error => {
-            console.log(error)
-        });
-    });
-
-    req.write(data)
-    req.end()
 }, 2000);
-
-
-
-
