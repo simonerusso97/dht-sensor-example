c6f3c6abc98b3ab26dd8baf07d4318add1e8c87f
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-10-06 17:02:54 +0200

    Initial commit

---
+++ sensor-test.js
@@ -0,0 +1,54 @@
+const sensorLib = require('node-dht-sensor');
+const http = require('http')
+
+
+// Setup sensor, exit if failed
+var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
+var sensorPin  = 4;  // The GPIO pin number for sensor signal
+if (!sensorLib.initialize(sensorType, sensorPin)) {
+    console.warn('Failed to initialize sensor');
+    process.exit(1);
+}
+
+// Automatically update sensor value every 2 seconds
+setInterval(function() {
+    var readout = sensorLib.read();
+    console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
+    console.log('Humidity:   ', readout.humidity.toFixed(1)    + '%');
+
+    const data = JSON.stringify({
+        "sensor": "ID1",
+        "timestamp": "0",
+        "temperature": readout.temperature.toFixed(1),
+    });
+
+    const options = {
+        hostname: '192.168.1.100', //TODO: cambia a seconda quale rete wifi uso
+        port: 3000,
+        path: '/temperature',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    };
+
+    const req = http.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`);
+
+        res.on('data', d => {
+            process.stdout.write(d)
+        });
+
+        req.on('error', error => {
+            console.log(error)
+        });
+    });
+
+    req.write(data)
+    req.end()
+}, 2000);
+
+
+
+
